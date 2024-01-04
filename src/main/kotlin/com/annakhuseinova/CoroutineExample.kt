62be41b9fa554b885425997740b4f5b2021f328e
Anna Khuseinova <annettekhuseinova@gmail.com>
2024-01-04 22:21:17 +0300

    added more examples

--- src/main/kotlin/com/annakhuseinova/CoroutineExample.kt
+++ src/main/kotlin/com/annakhuseinova/CoroutineExample.kt
@@ -1,6 +1,6 @@
 package com.annakhuseinova
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -23,9 +23,103 @@ suspend fun bathTime(){
 suspend fun boilingWater(){
     logger.info("Boiling water")
     delay(1000L)
-    logger.info("Water bolied")
+    logger.info("Water boiled")
+}
+
+suspend fun makeCoffee(){
+    logger.info("Starting to make coffee")
+    delay(500L)
+    logger.info("Done with coffee")
+}
+
+suspend fun prepareAndReturnJavaCoffee(): String {
+    logger.info("Starting to make coffee")
+    delay(500L)
+    logger.info("Done with coffee")
+    return "Java coffee"
+}
+
+suspend fun prepareAndReturnToastedBread(): String {
+    logger.info("Starting to make breakfast")
+    delay(1000L)
+    logger.info("Toast is out!")
+    return "Toasted bread"
+}
+
+suspend fun sequentialMorningRoutine(){
+    coroutineScope { // start a "context" for coroutines
+        bathTime()
+        // add more code including suspend functions
+        // Inside coroutine scope all functions are executed sequentially (unless launched with "launch")
+    }
+    coroutineScope {
+        boilingWater()
+    }
+}
+
+suspend fun concurrentMorningRoutine(){
+    coroutineScope {
+        launch { // launch function allows to launch parallel execution of coroutines (like new Thread(...).start())
+            bathTime()
+        }
+        // this coroutine is a CHILD of the coroutineScope. Can have nested child coroutines
+        launch { boilingWater()
+        }
+    }
+}
+
+// Launching stuff in a GlobalScope.launch won't provide structured concurrency. Coroutines will run independently, won't
+// have scope context
+suspend fun noStructConcurrencyMorningRoutine(){
+    GlobalScope.launch {
+        bathTime()
+    }
+    GlobalScope.launch {
+        boilingWater()
+    }
+}
+
+// Example of launching a coroutine with the guarantee that the 2 previous once are completed
+suspend fun morningRoutineWithCoffee(){
+    coroutineScope {
+        val bathTimeJob: Job = launch { bathTime() }
+        val boilingWaterJob: Job = launch { boilingWater() }
+        // join() is a semantic blocking method until the coroutine is done
+        bathTimeJob.join()
+        boilingWaterJob.join()
+        launch {
+            makeCoffee() // this function starts after bathTime() and boilingWater() finish
+        }
+    }
+}
+
+
+// structured concurrency allows to define in which order parallel computations will execute
+suspend fun morningCoroutineWithCoffeeStructured(){
+    coroutineScope {
+        coroutineScope {
+            launch { bathTime() }
+            launch { boilingWater() }
+        }
+        launch { makeCoffee() }
+    }
+}
+
+// Returning values from coroutines
+suspend fun prepareBreakfast(){
+    coroutineScope {
+        val coffee: Deferred<String> = async { prepareAndReturnJavaCoffee() } // Deferred<*> contains the result of the computation
+        // that will be filled once the computation is over. Analogous to Future in Java
+        val toast: Deferred<String> = async { prepareAndReturnToastedBread() }
+
+        // semantic blocking
+        val finalCoffee = coffee.await()
+        val finalToast = toast.await()
+        logger.info("I'm eating $finalToast and drinking $finalCoffee")
+    }
 }
 
 suspend fun main(args: Array<String>){
-    bathTime()
+    prepareBreakfast()
 }
+
